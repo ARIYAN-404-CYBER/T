609a5c9da06c938dcd1e99212c4d245de6ec5959
PICCI-ARIYAN <116664645+ARIYAN-404-CYBER@users.noreply.github.com>
2023-02-03 01:58:15 +0600

    Create SEX1X1.py

---
+++ SEX1X1.py
@@ -0,0 +1,28 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+import requests
+
+print('[✓]CONGRATULATIONS YOUR DEVICE SUPPORT THIS TOOL') 
+
+print('[✓] LOADING...')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from SEX1X1 import SEX1XX
+
+    SEX1X1()
+
+
+
+
+
